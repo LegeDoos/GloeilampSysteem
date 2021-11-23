446cdd5b3d03f0e7019279c56dfbefc10396b10d
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2021-11-23 13:34:59 +0100

    Add project files.

---
+++ Dimschakelaar.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GloeilampSysteem
+{
+    class Dimschakelaar : Schakelaar
+    {
+
+        /// <summary>
+        /// De stand waarin de schakelaar zich bevindt, 0..4
+        /// </summary>
+        private int Stand { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Dimschakelaar() : base() //let op: base constructor wordt ook uitgevoerd
+        {
+            this.Stand = 0;
+            UitgaandeSpanning = this.Stand * 55;
+        }
+        
+
+        /// <summary>
+        /// Verlaag de dimmer met een stand
+        /// </summary>
+        public void Lager()
+        {
+            Stand = Stand > 0 ? Stand-1 : 0;
+            UitgaandeSpanning = this.Stand * 55;
+            UpdateSpanningOpLampen();
+        }
+
+        /// <summary>
+        /// Verhoog de dimmer met een stand
+        /// </summary>
+        public void Hoger()
+        {
+            Stand = Stand < 4 ? Stand+1 : 4;
+            UitgaandeSpanning = this.Stand * 55;
+            UpdateSpanningOpLampen();
+        }
+
+        private void UpdateSpanningOpLampen()
+        {
+            foreach (var lamp in Lampen)
+            {
+                lamp.ZetSpanningOpPolen(UitgaandeSpanning);
+            }
+        }
+    }
+}
---
+++ Schakelaar.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GloeilampSysteem
+{
+    class Schakelaar
+    {
+        //properties
+        /// <summary>
+        /// Is het circuit gesloten (true) of open (false)
+        /// </summary>
+        public Boolean Ingeschakeld { get; private set; }
+
+        /// <summary>
+        /// Wat is de uitgaande spanning van de schakelaar?
+        /// </summary>
+        protected int UitgaandeSpanning { get; set; }
+
+        /// <summary>
+        /// Welke lampen worden bediend
+        /// </summary>
+        protected List<Lamp> Lampen = new List<Lamp>();
+
+        /// <summary>
+        /// Constructor van de class: wordt gebruikt om het object wat wordt gemaakt te instantieren
+        /// </summary>
+        public Schakelaar()
+        {
+            this.Ingeschakeld = false;
+            this.UitgaandeSpanning = 220;
+        }
+
+        /// <summary>
+        /// Koppel een lamp aan de schakelaar
+        /// </summary>
+        /// <param name="_lamp">De lamp om te koppelen</param>
+        public void VoegLampToe(Lamp _lamp)
+        {
+            Lampen.Add(_lamp);
+        }
+
+        /// <summary>
+        /// Toggle de schakelaar en zet de lampen aan of uit
+        /// </summary>
+        public void Toggle()
+        {
+            if (this.Ingeschakeld) // de schakelaar staat aan
+            {
+                this.Ingeschakeld = false;
+                foreach (var lamp in this.Lampen)
+                {
+                    lamp.ZetSpanningOpPolen(0);
+                }
+            }
+            else // de schakelaar staat uit
+            {
+                this.Ingeschakeld = true;
+                foreach (var lamp in this.Lampen)
+                {
+                    lamp.ZetSpanningOpPolen(this.UitgaandeSpanning);
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Toon de status van de lampen in de console
+        /// </summary>
+        public void ToonStatus()
+        {
+            Console.WriteLine($"De schakelaar heeft status {(this.Ingeschakeld ? "aan" : "uit")} en heeft een spanning van {this.UitgaandeSpanning} Volt");
+            foreach (var lamp in this.Lampen)
+            {
+                Console.WriteLine(lamp.ToonStatus());
+            }
+            Console.WriteLine();
+        }
+    }
+}
