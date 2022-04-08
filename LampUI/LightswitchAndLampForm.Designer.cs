ffdf78f92ade750f54e85da08f77438dc7df7977
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2022-04-08 20:45:43 +0200

    Fix navigation between grids and make sure the correct item is selected,

--- LampUI/LightswitchAndLampForm.Designer.cs
+++ LampUI/LightswitchAndLampForm.Designer.cs
@@ -41,6 +41,7 @@
             this.lampButtonsGroupBox = new System.Windows.Forms.GroupBox();
             this.deleteLampButton = new System.Windows.Forms.Button();
             this.createLampButton = new System.Windows.Forms.Button();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.statusStrip.SuspendLayout();
             this.lightswitchGroupBox.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.lightswitchDataGridView)).BeginInit();
@@ -54,18 +55,18 @@
             // 
             this.statusStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.currentDalStatusLabel});
-            this.statusStrip.Location = new System.Drawing.Point(0, 574);
+            this.currentDalStatusLabel,
+            this.toolStripStatusLabel1});
+            this.statusStrip.Location = new System.Drawing.Point(0, 428);
             this.statusStrip.Name = "statusStrip";
-            this.statusStrip.Padding = new System.Windows.Forms.Padding(1, 0, 16, 0);
-            this.statusStrip.Size = new System.Drawing.Size(914, 26);
+            this.statusStrip.Size = new System.Drawing.Size(800, 22);
             this.statusStrip.TabIndex = 0;
             this.statusStrip.Text = "statusStrip1";
             // 
             // currentDalStatusLabel
             // 
             this.currentDalStatusLabel.Name = "currentDalStatusLabel";
-            this.currentDalStatusLabel.Size = new System.Drawing.Size(96, 20);
+            this.currentDalStatusLabel.Size = new System.Drawing.Size(78, 17);
             this.currentDalStatusLabel.Text = "Current DAL: ";
             // 
             // lightswitchGroupBox
@@ -74,10 +75,8 @@
             this.lightswitchGroupBox.Controls.Add(this.lightswitchButtonsGroupBox);
             this.lightswitchGroupBox.Dock = System.Windows.Forms.DockStyle.Left;
             this.lightswitchGroupBox.Location = new System.Drawing.Point(0, 0);
-            this.lightswitchGroupBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.lightswitchGroupBox.Name = "lightswitchGroupBox";
-            this.lightswitchGroupBox.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.lightswitchGroupBox.Size = new System.Drawing.Size(447, 574);
+            this.lightswitchGroupBox.Size = new System.Drawing.Size(391, 428);
             this.lightswitchGroupBox.TabIndex = 1;
             this.lightswitchGroupBox.TabStop = false;
             this.lightswitchGroupBox.Text = "Lightswitches";
@@ -86,12 +85,16 @@
             // 
             this.lightswitchDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.lightswitchDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lightswitchDataGridView.Location = new System.Drawing.Point(3, 24);
+            this.lightswitchDataGridView.Location = new System.Drawing.Point(3, 19);
+            this.lightswitchDataGridView.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lightswitchDataGridView.MultiSelect = false;
             this.lightswitchDataGridView.Name = "lightswitchDataGridView";
             this.lightswitchDataGridView.ReadOnly = true;
             this.lightswitchDataGridView.RowHeadersWidth = 51;
-            this.lightswitchDataGridView.Size = new System.Drawing.Size(441, 465);
+            this.lightswitchDataGridView.Size = new System.Drawing.Size(385, 345);
             this.lightswitchDataGridView.TabIndex = 1;
+            this.lightswitchDataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.lightswitchDataGridView_CellClick);
+            this.lightswitchDataGridView.SelectionChanged += new System.EventHandler(this.lightswitchDataGridView_SelectionChanged);
             // 
             // lightswitchButtonsGroupBox
             // 
@@ -99,35 +102,41 @@
             this.lightswitchButtonsGroupBox.Controls.Add(this.deleteLightswitchButton);
             this.lightswitchButtonsGroupBox.Controls.Add(this.createLightSwitchButton);
             this.lightswitchButtonsGroupBox.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.lightswitchButtonsGroupBox.Location = new System.Drawing.Point(3, 489);
+            this.lightswitchButtonsGroupBox.Location = new System.Drawing.Point(3, 364);
+            this.lightswitchButtonsGroupBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.lightswitchButtonsGroupBox.Name = "lightswitchButtonsGroupBox";
-            this.lightswitchButtonsGroupBox.Size = new System.Drawing.Size(441, 81);
+            this.lightswitchButtonsGroupBox.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lightswitchButtonsGroupBox.Size = new System.Drawing.Size(385, 61);
             this.lightswitchButtonsGroupBox.TabIndex = 0;
             this.lightswitchButtonsGroupBox.TabStop = false;
             // 
             // toggleLightswitchButton
             // 
-            this.toggleLightswitchButton.Location = new System.Drawing.Point(263, 26);
+            this.toggleLightswitchButton.Location = new System.Drawing.Point(230, 20);
+            this.toggleLightswitchButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.toggleLightswitchButton.Name = "toggleLightswitchButton";
-            this.toggleLightswitchButton.Size = new System.Drawing.Size(94, 29);
+            this.toggleLightswitchButton.Size = new System.Drawing.Size(82, 22);
             this.toggleLightswitchButton.TabIndex = 2;
             this.toggleLightswitchButton.Text = "Toggle";
             this.toggleLightswitchButton.UseVisualStyleBackColor = true;
             // 
             // deleteLightswitchButton
             // 
-            this.deleteLightswitchButton.Location = new System.Drawing.Point(163, 26);
+            this.deleteLightswitchButton.Location = new System.Drawing.Point(143, 20);
+            this.deleteLightswitchButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.deleteLightswitchButton.Name = "deleteLightswitchButton";
-            this.deleteLightswitchButton.Size = new System.Drawing.Size(94, 29);
+            this.deleteLightswitchButton.Size = new System.Drawing.Size(82, 22);
             this.deleteLightswitchButton.TabIndex = 1;
             this.deleteLightswitchButton.Text = "Delete";
             this.deleteLightswitchButton.UseVisualStyleBackColor = true;
+            this.deleteLightswitchButton.Click += new System.EventHandler(this.deleteLightswitchButton_Click);
             // 
             // createLightSwitchButton
             // 
-            this.createLightSwitchButton.Location = new System.Drawing.Point(63, 26);
+            this.createLightSwitchButton.Location = new System.Drawing.Point(55, 20);
+            this.createLightSwitchButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.createLightSwitchButton.Name = "createLightSwitchButton";
-            this.createLightSwitchButton.Size = new System.Drawing.Size(94, 29);
+            this.createLightSwitchButton.Size = new System.Drawing.Size(82, 22);
             this.createLightSwitchButton.TabIndex = 0;
             this.createLightSwitchButton.Text = "Create";
             this.createLightSwitchButton.UseVisualStyleBackColor = true;
@@ -138,11 +147,9 @@
             this.lampsGroupBox.Controls.Add(this.lampsDataGridView);
             this.lampsGroupBox.Controls.Add(this.lampButtonsGroupBox);
             this.lampsGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lampsGroupBox.Location = new System.Drawing.Point(447, 0);
-            this.lampsGroupBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.lampsGroupBox.Location = new System.Drawing.Point(391, 0);
             this.lampsGroupBox.Name = "lampsGroupBox";
-            this.lampsGroupBox.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.lampsGroupBox.Size = new System.Drawing.Size(467, 574);
+            this.lampsGroupBox.Size = new System.Drawing.Size(409, 428);
             this.lampsGroupBox.TabIndex = 2;
             this.lampsGroupBox.TabStop = false;
             this.lampsGroupBox.Text = "Lamps";
@@ -153,53 +160,66 @@
             this.lampsDataGridView.AllowUserToDeleteRows = false;
             this.lampsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.lampsDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lampsDataGridView.Location = new System.Drawing.Point(3, 24);
+            this.lampsDataGridView.Location = new System.Drawing.Point(3, 19);
+            this.lampsDataGridView.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.lampsDataGridView.Name = "lampsDataGridView";
             this.lampsDataGridView.ReadOnly = true;
             this.lampsDataGridView.RowHeadersWidth = 51;
             this.lampsDataGridView.RowTemplate.Height = 29;
-            this.lampsDataGridView.Size = new System.Drawing.Size(461, 465);
+            this.lampsDataGridView.Size = new System.Drawing.Size(403, 345);
             this.lampsDataGridView.TabIndex = 2;
+            this.lampsDataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.lampsDataGridView_CellClick);
+            this.lampsDataGridView.SelectionChanged += new System.EventHandler(this.lampsDataGridView_SelectionChanged);
             // 
             // lampButtonsGroupBox
             // 
             this.lampButtonsGroupBox.Controls.Add(this.deleteLampButton);
             this.lampButtonsGroupBox.Controls.Add(this.createLampButton);
             this.lampButtonsGroupBox.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.lampButtonsGroupBox.Location = new System.Drawing.Point(3, 489);
+            this.lampButtonsGroupBox.Location = new System.Drawing.Point(3, 364);
+            this.lampButtonsGroupBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.lampButtonsGroupBox.Name = "lampButtonsGroupBox";
-            this.lampButtonsGroupBox.Size = new System.Drawing.Size(461, 81);
+            this.lampButtonsGroupBox.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lampButtonsGroupBox.Size = new System.Drawing.Size(403, 61);
             this.lampButtonsGroupBox.TabIndex = 1;
             this.lampButtonsGroupBox.TabStop = false;
             // 
             // deleteLampButton
             // 
-            this.deleteLampButton.Location = new System.Drawing.Point(233, 26);
+            this.deleteLampButton.Location = new System.Drawing.Point(204, 20);
+            this.deleteLampButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.deleteLampButton.Name = "deleteLampButton";
-            this.deleteLampButton.Size = new System.Drawing.Size(94, 29);
+            this.deleteLampButton.Size = new System.Drawing.Size(82, 22);
             this.deleteLampButton.TabIndex = 3;
             this.deleteLampButton.Text = "Delete";
             this.deleteLampButton.UseVisualStyleBackColor = true;
+            this.deleteLampButton.Click += new System.EventHandler(this.deleteLampButton_Click);
             // 
             // createLampButton
             // 
-            this.createLampButton.Location = new System.Drawing.Point(133, 26);
+            this.createLampButton.Location = new System.Drawing.Point(116, 20);
+            this.createLampButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.createLampButton.Name = "createLampButton";
-            this.createLampButton.Size = new System.Drawing.Size(94, 29);
+            this.createLampButton.Size = new System.Drawing.Size(82, 22);
             this.createLampButton.TabIndex = 2;
             this.createLampButton.Text = "Create";
             this.createLampButton.UseVisualStyleBackColor = true;
             this.createLampButton.Click += new System.EventHandler(this.createLampButton_Click);
             // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
             // LightswitchAndLampForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(914, 600);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.lampsGroupBox);
             this.Controls.Add(this.lightswitchGroupBox);
             this.Controls.Add(this.statusStrip);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "LightswitchAndLampForm";
             this.Text = "Lightswitch and lamp";
             this.statusStrip.ResumeLayout(false);
@@ -230,5 +250,6 @@
         private Button deleteLightswitchButton;
         private Button deleteLampButton;
         private Button createLampButton;
+        private ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
--- LampUI/LightswitchAndLampForm.cs
+++ LampUI/LightswitchAndLampForm.cs
@@ -4,6 +4,9 @@ namespace LampUI
 {
     public partial class LightswitchAndLampForm : Form
     {
+        Lightswitch selectedLightswitch;
+        Lamp selectedLamp;
+
         public LightswitchAndLampForm()
         {
             InitializeComponent();
@@ -13,9 +16,34 @@ namespace LampUI
 
         private void ReadData()
         {
-            lightswitchDataGridView.DataSource = Lightswitch.Read();
-            
+            var switches = Lightswitch.Read();
+            lightswitchDataGridView.DataSource = switches;
+            RefreshData();
         }
+
+        private void RefreshData()
+        {
+
+            if (lightswitchDataGridView.Rows.Count > 0
+                && (lightswitchDataGridView.SelectedRows.Count > 0 || lightswitchDataGridView.SelectedCells.Count > 0))
+            {
+                // update selected lightswitch
+                if (selectedLightswitch != lightswitchDataGridView.CurrentRow.DataBoundItem as Lightswitch)
+                {
+                    selectedLightswitch = lightswitchDataGridView.CurrentRow.DataBoundItem as Lightswitch;
+                    lampsDataGridView.DataSource = selectedLightswitch.Lamps;
+                }
+            }
+
+            if (lampsDataGridView.Rows.Count > 0
+                && (lampsDataGridView.SelectedRows.Count > 0 || lampsDataGridView.SelectedCells.Count > 0))
+            {
+                // update selected lamp
+                selectedLamp = lampsDataGridView.CurrentRow.DataBoundItem as Lamp;
+            }
+
+        }
+
 
         private void createLightSwitchButton_Click(object sender, EventArgs e)
         {
@@ -28,6 +56,36 @@ namespace LampUI
         {
             var dialog = new GetNameDialog();
             dialog.ShowDialog();
+        }
+
+        private void lightswitchDataGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void lightswitchDataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void deleteLightswitchButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"{selectedLightswitch.Name}");
+        }
+
+        private void deleteLampButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"{selectedLamp.Name}");
+        }
+
+        private void lampsDataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void lampsDataGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
